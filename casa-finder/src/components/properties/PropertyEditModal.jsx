f637974fe1edf223382672d713625b7d50461e65
Cristopher Rondon Añaños <89040296+Cris200399@users.noreply.github.com>
2024-10-23 22:39:30 -0500

    code: fix editModalProperty

--- casa-finder/src/components/properties/PropertyEditModal.jsx
+++ casa-finder/src/components/properties/PropertyEditModal.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { FaStar, FaStarHalfAlt } from 'react-icons/fa';
+import React, {useEffect, useRef, useState} from 'react';
+import {FaStar, FaStarHalfAlt} from 'react-icons/fa';
 
-const PropertyModal = ({ property, onClose }) => {
+const PropertyModal = ({property, onClose}) => {
     const formRef = useRef(null);
 
     const handleClickOutside = (e) => {
@@ -22,7 +22,7 @@ const PropertyModal = ({ property, onClose }) => {
             <div ref={formRef} className="bg-gray-200 rounded-lg shadow-lg max-w-4xl w-full p-6">
                 {/* Modal Header */}
                 <div className="flex justify-between items-center">
-                    <h2 className="text-2xl font-semibold">Condominio Pradera</h2>
+                    <h2 className="text-2xl font-semibold">{property.name}</h2>
                     <button onClick={onClose} className="text-gray-500 hover:text-gray-700">
                         &#x2715;
                     </button>
@@ -31,15 +31,15 @@ const PropertyModal = ({ property, onClose }) => {
                 {/* Imagen y detalles principales */}
                 <div className="grid grid-cols-2 gap-4 mt-4">
                     <div className="bg-gray-300 w-full h-48 flex items-center justify-center">
-                        <img className="w-full h-32 object-cover rounded" src={property.image} alt={property.name} />
+                        <img className="w-full h-32 object-cover rounded" src={property.image} alt={property.name}/>
                     </div>
                     <div className="text-gray-700">
                         <p className="text-3xl font-semibold">S/ 25,500.00</p>
                         <div className="flex items-center my-2">
                             {[...Array(4)].map((_, i) => (
-                                <FaStar key={i} className="text-yellow-400" />
+                                <FaStar key={i} className="text-yellow-400"/>
                             ))}
-                            <FaStarHalfAlt className="text-yellow-400" />
+                            <FaStarHalfAlt className="text-yellow-400"/>
                         </div>
                         <p>Pisos: {property.floors} {property.floors > 1 ? "pisos" : "piso"}</p>
                         <p>Tipo: {property.type}</p>
@@ -63,19 +63,19 @@ const PropertyModal = ({ property, onClose }) => {
                     <h3 className="font-semibold text-xl">¿Por qué es una buena elección?</h3>
                     <div className="grid grid-cols-2 gap-4 mt-2">
                         <div className="flex items-center">
-                            <input type="checkbox" checked readOnly className="mr-2" />
+                            <input type="checkbox" checked readOnly className="mr-2"/>
                             Cerca al colegio Ingenieros
                         </div>
                         <div className="flex items-center">
-                            <input type="checkbox" checked readOnly className="mr-2" />
+                            <input type="checkbox" checked readOnly className="mr-2"/>
                             Cerca a parques
                         </div>
                         <div className="flex items-center">
-                            <input type="checkbox" checked readOnly className="mr-2" />
+                            <input type="checkbox" checked readOnly className="mr-2"/>
                             Cerca al centro comercial Real Plaza
                         </div>
                         <div className="flex items-center">
-                            <input type="checkbox" checked readOnly className="mr-2" />
+                            <input type="checkbox" checked readOnly className="mr-2"/>
                             Mucha vegetación
                         </div>
                     </div>
@@ -86,19 +86,19 @@ const PropertyModal = ({ property, onClose }) => {
                     <h3 className="font-semibold text-xl">¿Qué incluye en el precio?</h3>
                     <div className="grid grid-cols-2 gap-4 mt-2">
                         <div className="flex items-center">
-                            <input type="checkbox" checked readOnly className="mr-2" />
+                            <input type="checkbox" checked readOnly className="mr-2"/>
                             Reja protectora
                         </div>
                         <div className="flex items-center">
-                            <input type="checkbox" checked readOnly className="mr-2" />
+                            <input type="checkbox" checked readOnly className="mr-2"/>
                             Áreas verdes
                         </div>
                         <div className="flex items-center">
-                            <input type="checkbox" checked readOnly className="mr-2" />
+                            <input type="checkbox" checked readOnly className="mr-2"/>
                             Guardias vigilantes 24 horas
                         </div>
                         <div className="flex items-center">
-                            <input type="checkbox" checked readOnly className="mr-2" />
+                            <input type="checkbox" checked readOnly className="mr-2"/>
                             Pet friendly
                         </div>
                     </div>
