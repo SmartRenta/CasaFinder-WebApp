1895b2ae2e1f44c213002b199c47429c8f47f4ba
grecia <greciaguerreroa@gmail.com>
2024-10-28 18:02:08 -0500

    rebase notifications bar

---
+++ casa-finder/src/components/shared/Notifications/NotificationBar.jsx
@@ -0,0 +1,48 @@
+import React, {useEffect, useRef, useState} from "react";
+import axios from 'axios';
+import NotificationCard from "./NotificationCard";
+//import data from './././data/notifications.json';
+
+const NotificationBar = ({onClose}) => {
+    
+    const formRef = useRef(null);
+    const handleClickOutside = (e) => {
+        if (formRef.current && !formRef.current.contains(e.target)) {
+            onClose();
+        }
+    };
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+    const [notificationsData, setnotificationsData] = useState('');
+    useEffect(()=>{
+        const fetchData =  async() =>{
+            const {data} = await axios.get('notifications.json');
+            setnotificationsData(data);
+        }
+        fetchData();
+    }, []);
+    
+    //inset-0 flex items-center justify-center   bg-gray-800 
+    return (
+        <div className="fixed inset-y-0 right-0 mt-6 bg-opacity-50">
+            <div
+                ref={formRef}
+                className="w-64 h-96 overflow-y-auto mt-6 bg-gray-700 text-white text-black p-4 shadow-lg border rounded"
+            >
+                {Object.keys(notificationsData).reverse().map((notification) => (
+                    <div className="col-4">
+                        <NotificationCard notification={notificationsData[notification]
+                        }/>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default NotificationBar;
