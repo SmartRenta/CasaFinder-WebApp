a99960292d85f1f781cafc7a814b0f04d0044da8
grecia <greciaguerreroa@gmail.com>
2024-10-30 10:03:03 -0500

    add notifications redirection

--- casa-finder/src/components/shared/Notifications/NotificationBar.jsx
+++ casa-finder/src/components/shared/Notifications/NotificationBar.jsx
@@ -1,11 +1,18 @@
 import React, {useEffect, useRef, useState} from "react";
-import axios from 'axios';
+import { useNavigate } from "react-router-dom"; 
+//import axios from 'axios';
 import NotificationCard from "./NotificationCard";
-//import data from './././data/notifications.json';
+import { getUserRoleFromCache, setUserRoleInCache } from "../../../utils/authUtils";
+import notificationsLandlord from "../../../data/notificationsLandlord.json";
+import notificationsTenant from "../../../data/notificationsTenant.json";
 
 const NotificationBar = ({onClose}) => {
     
+    const userRole = getUserRoleFromCache();
+    const data = (userRole === "tenant" ? notificationsTenant : notificationsLandlord);
     const formRef = useRef(null);
+    const navigate = useNavigate();
+
     const handleClickOutside = (e) => {
         if (formRef.current && !formRef.current.contains(e.target)) {
             onClose();
@@ -17,15 +24,15 @@ const NotificationBar = ({onClose}) => {
             document.removeEventListener("mousedown", handleClickOutside);
         };
     }, []);
-
-    const [notificationsData, setnotificationsData] = useState('');
+    
+    /*const [notificationsData, setnotificationsData] = useState('');
     useEffect(()=>{
         const fetchData =  async() =>{
-            const {data} = await axios.get('notifications.json');
+            const {data} = await axios.get('/notifications.json');
             setnotificationsData(data);
         }
         fetchData();
-    }, []);
+    }, []);*/
     
     const handleMouseEnter = (e) => {
         e.currentTarget.style.background = '#4B5563'; //'#9CA3AF';
@@ -34,20 +41,17 @@ const NotificationBar = ({onClose}) => {
     const handleMouseLeave = (e) => {
         e.currentTarget.style.background = '#374151';
     }
-    const handleClick = (e) => {
-        console.log("click");
+    const handleClick = (notification) => {
+        onClose();
+        navigate(`/notifications/${notification.id}`); 
     }
 
-    //inset-0 flex items-center justify-center   bg-gray-800 
     return (
         <div className="fixed inset-y-0 right-0 mt-6 bg-opacity-50">
-            <div
-                ref={formRef}
-                className="w-64 h-96 overflow-y-auto mt-6 bg-gray-700 text-white text-black p-4 shadow-lg border rounded"
-            >
-                {Object.keys(notificationsData).reverse().map((notification) => (
-                    <div className="col-4" onClick={handleClick} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-                        <NotificationCard notification={notificationsData[notification]}/>
+            <div ref={formRef} className="w-64 h-96 overflow-y-auto mt-6 bg-gray-700 text-white text-black p-4 shadow-lg border rounded">
+                {data.reverse().map((notification) => (
+                    <div key={notification.id} className="col-4" onClick={() => handleClick(notification)} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                        <NotificationCard notification={notification}/>
                     </div>
                 ))}
             </div>
